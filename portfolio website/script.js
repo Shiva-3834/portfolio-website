72bb94d52c46bf9c5035997b665a04bbab993625
Shiva-3834 <111730040+Shiva-3834@users.noreply.github.com>
2022-10-04 01:45:16 +0530

    Add files via upload

---
+++ portfolio website/script.js
@@ -0,0 +1,7 @@
+/* ======= typing animatiom =====*/
+var typed = new typed(".typing",{
+strings: ["","web Designer" , "web Developer" , "Graphic Design Designer "],
+typeSpeed: 100,
+BackSpeed:60,
+loop:true
+})
